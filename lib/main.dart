b8cbbf76627d3472d6e3535d986333d6980e6dde
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-09 15:45:44 +0200

    Ajout de Card pour les boulangeries

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,133 @@ class MyApp extends StatelessWidget {
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.shopping_cart),
-              onPressed: () {},
-            )
+              onPressed: () {}),
           ],
           backgroundColor: Colors.green,
           leading: IconButton(
             icon: const Icon(Icons.menu),
-            onPressed: () {},
+            onPressed: () {}),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 20,
+              children: [
+                Card(
+                  elevation: 10,
+                  color: Colors.grey[300],
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Container(
+                          width: 225,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "M ton pain",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(1),
+                        ),
+                        Image.asset(
+                          "lib/img.jpg",
+                          width: 250,
+                          height: 130,
+                        ),
+                        Padding(padding: EdgeInsets.all(5))
+                      ],
+                    ),
+                  ),
+                ),
+
+                Card(
+                  elevation: 10,
+                  color: Colors.grey[300],
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Container(
+                          width: 225,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "M ton pain",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(1),
+                        ),
+                        Image.asset(
+                          "lib/img.jpg",
+                          width: 250,
+                          height: 130,
+                        ),
+                        Padding(padding: EdgeInsets.all(5))
+                      ],
+                    ),
+                  ),
+                ),
+
+                Card(
+                  elevation: 10,
+                  color: Colors.grey[300],
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Container(
+                          width: 225,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "M ton pain",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(1),
+                        ),
+                        Image.asset(
+                          "lib/img.jpg",
+                          width: 250,
+                          height: 130,
+                        ),
+                        Padding(padding: EdgeInsets.all(5))
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
