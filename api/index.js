fbdbab6e36fb81abc77e632a39272f724705db05
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-23 21:30:39 +0200

    Déploiement de l'API sur Railway

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
+const express = require('express');
 const mysql = require('mysql2/promise');
 const url = require('url');
 
+const app = express();
+
 const MYSQL_PUBLIC_URL = process.env.MYSQL_PUBLIC_URL;
+const API_PORT = process.env.API_PORT;
 
 const params = url.parse(MYSQL_PUBLIC_URL);
 const [user, password] = params.auth.split(':');
@@ -21,25 +25,32 @@ async function getData() {
 
     try {
         
-        const rows = await connection.execute('SELECT * FROM boulangerie WHERE id = ?', [1]);
+        const rows = await connection.execute('SELECT * FROM boulangerie');
 
         if (rows.length === 0) {
             console.log("Aucun utilisateur trouvé avec cet ID");
         } 
         
         else {
-            console.log(rows[0]);
+            return rows[0];
         }
     } 
     
     catch (err) {
         console.error('Erreur lors de la requête:', err);
+        return [];
     } 
     
     finally {
         await connection.end();
-        console.log("Connexion terminée");
     }
 }
 
-getData();
+app.get("/boulangerie", async (req, res) => {
+    const data = await getData()
+    res.json(data);
+})
+
+app.listen(API_PORT, () => {
+    console.log(`Server running on port ${API_PORT}`);
+})
