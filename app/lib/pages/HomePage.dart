62bfc85ce8b629c77a742ab40cb3a4136b9a6bda
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-23 16:51:57 +0200

    Factorisation du code grace a une méthode buildCard()

--- app/lib/pages/HomePage.dart
+++ app/lib/pages/HomePage.dart
@@ -35,97 +35,43 @@ class HomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 spacing: 20,
                 children: [
-                  Card(
-                    elevation: 10,
-                    color: Colors.grey[300],
-                    child: Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(padding: EdgeInsets.all(5)),
-                          Container(
-                            width: 225,
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "M ton pain",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Padding(padding: EdgeInsets.all(1)),
-                          Image.asset("lib/img.jpg", width: 250, height: 130),
-                          Padding(padding: EdgeInsets.all(5)),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  Card(
-                    elevation: 10,
-                    color: Colors.grey[300],
-                    child: Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(padding: EdgeInsets.all(5)),
-                          Container(
-                            width: 225,
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "M ton pain",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Padding(padding: EdgeInsets.all(1)),
-                          Image.asset("lib/img.jpg", width: 250, height: 130),
-                          Padding(padding: EdgeInsets.all(5)),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  Card(
-                    elevation: 10,
-                    color: Colors.grey[300],
-                    child: Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(padding: EdgeInsets.all(5)),
-                          Container(
-                            width: 225,
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "M ton pain",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Padding(padding: EdgeInsets.all(1)),
-                          Image.asset("lib/img.jpg", width: 250, height: 130),
-                          Padding(padding: EdgeInsets.all(5)),
-                        ],
-                      ),
-                    ),
-                  ),
+                  buildCard(),
+                  buildCard(),
+                  buildCard(),
+                  buildCard(),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget buildCard() {
+    return Card(
+      elevation: 10,
+      color: Colors.grey[300],
+      child: Padding(
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(5)),
+            Container(
+              width: 225,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "M ton pain",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(1)),
+            Image.asset("lib/img.jpg", width: 250, height: 130),
+            Padding(padding: EdgeInsets.all(5)),
+          ],
+        ),
       ),
     );
   }
