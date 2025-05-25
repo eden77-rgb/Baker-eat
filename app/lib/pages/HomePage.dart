caf1e61cb6286c674475d740871c6c57bed61597
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-25 19:53:17 +0200

    Ajout dynamique du nom et de l'image selon id

--- app/lib/pages/HomePage.dart
+++ app/lib/pages/HomePage.dart
@@ -13,7 +13,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    nom = DbService().getNom(1);
   }
 
   @override
@@ -28,30 +27,82 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.green,
         leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
       ),
-      body: FutureBuilder<String>(
-        future: nom,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } 
-          
-          else if (snapshot.hasError) {
-            return Center(child: Text("Erreur : ${snapshot.error}"));
-          } 
-          
-          else {
-            final data = snapshot.data!;
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(3)),
 
-            return SingleChildScrollView(
+            Container(
+              width: 350,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Boulangerie",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 20,
+                  children: [
+                    buildCard(1),
+                    buildCard(2),
+                    buildCard(3),
+                    buildCard(4),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCard(int id) {
+    return FutureBuilder<List<String>>(
+      future: Future.wait([DbService.getNom(id), DbService.getImg(id)]),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox(
+            width: 250,
+            height: 180,
+            child: Center(child: CircularProgressIndicator()),
+          );
+        } 
+        
+        else if (snapshot.hasError) {
+          return SizedBox(
+            width: 250,
+            height: 180,
+            child: Center(child: Text("Erreur: ${snapshot.error}")),
+          );
+        } 
+        
+        else {
+          final nom = snapshot.data![0];
+          final image = snapshot.data![1];
+
+          return Card(
+            elevation: 10,
+            color: Colors.grey[300],
+            child: Padding(
+              padding: const EdgeInsets.all(0),
               child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.all(3)),
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.all(5)),
 
                   Container(
-                    width: 350,
+                    width: 225,
                     alignment: Alignment.centerLeft,
                     child: Text(
-                      "Catégorie",
+                      nom,
                       textAlign: TextAlign.left,
                       style: TextStyle(
                         fontSize: 20,
@@ -60,60 +111,17 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
 
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        spacing: 20,
-                        children: [
-                          buildCard(data),
-                          buildCard(data),
-                          buildCard(data),
-                          buildCard(data),
-                        ],
-                      ),
-                    ),
-                  ),
+                  Padding(padding: EdgeInsets.all(1)),
+
+                  Image.network(image, width: 250, height: 130),
+
+                  Padding(padding: EdgeInsets.all(5)),
                 ],
               ),
-            );
-          }
-        },
-      ),
-    );
-  }
-
-  Widget buildCard(String nom) {
-    return Card(
-      elevation: 10,
-      color: Colors.grey[300],
-      child: Padding(
-        padding: const EdgeInsets.all(0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.all(5)),
-
-            Container(
-              width: 225,
-              alignment: Alignment.centerLeft,
-              child: Text(
-                nom,
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
             ),
-
-            Padding(padding: EdgeInsets.all(1)),
-
-            Image.asset("lib/img.jpg", width: 250, height: 130),
-
-            Padding(padding: EdgeInsets.all(5)),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
--- app/lib/services/DBService.dart
+++ app/lib/services/DBService.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class DbService {
 
-  Future<List<dynamic>> getData(id) async {
+  static Future<List<dynamic>> getData(id) async {
     final reponse = await http.get(Uri.parse("https://api-nodejs-production-c1fe.up.railway.app/boulangerie/getDataId?id=${id.toString()}"));
   
     if (reponse.statusCode == 200) {
@@ -17,9 +17,15 @@ class DbService {
     }
   }
 
-  Future<String> getNom(int id) async {
+  static Future<String> getNom(int id) async {
     final List<dynamic> jsonData = await getData(id);
 
     return jsonData[0]["nom"];
+  }
+
+  static Future<String> getImg(int id) async {
+    final List<dynamic> jsonData = await getData(id);
+
+    return jsonData[0]["image"];
   }
 }
